d1aec7eed220026b98a2bdc7afaa989953fac1e2
Vino Samari <iamvinomusic@gmail.com>
2021-09-13 18:42:53 +0000

    DB config

--- config/database.js
+++ config/database.js
@@ -4,19 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env(
-          "DATABASE_HOST",
-          "serahjohnsoncluster.o6ice.mongodb.net/test"
-        ),
-        // srv: env.bool("DATABASE_SRV", true),
+        host: env("DATABASE_HOST", "serahjohnsoncluster.uemjx.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
         database: env("DATABASE_NAME", "serahjohnsonbackend"),
-        // username: env("DATABASE_USERNAME", "sapwavino"),
-        // password: env("DATABASE_PASSWORD", "blackbeat"),
+        username: env("DATABASE_USERNAME", "sapwavino"),
+        password: env("DATABASE_PASSWORD", "blackbeat"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        // ssl: env.bool("DATABASE_SSL", true),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
