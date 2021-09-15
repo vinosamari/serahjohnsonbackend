d9a9e1e767a386c60c933e5402b4ed2f3725b879
Vino Samari <iamvinomusic@gmail.com>
2021-09-15 20:03:43 +0000

    Fixed email on submit

--- api/newsletter/controllers/newsletter.js
+++ api/newsletter/controllers/newsletter.js
@@ -6,16 +6,19 @@ module.exports = {
     let entity;
     if (ctx.is("multipart")) {
       const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.live_alerts.create(data, { files });
+      entity = await strapi.services.newsletter.create(data, { files });
     } else {
-      entity = await strapi.services.live_alerts.create(ctx.request.body);
+      console.log("Entering");
+
+      entity = await strapi.services.newsletter.create(ctx.request.body);
       await strapi.plugins["email"].services.email.send({
         to: `${ctx.request.body.email}`,
         subject: `🎉🎉🎉  Hey ${ctx.request.body.name} Let's Bring Your Ideas To Life TODAY! 🎉🎉🎉`,
         text: `Hello ${ctx.request.body.name}, I'm Extremely Happy to Have You Here At The Start of A New Chapter. Our Journey Has Only Just Begun and I'll Be Right Beside You Telling The Story of This Wonderful Experience. Feel Free To Reach Out To Me With Your Ideas & Projects`,
-        html: `Welcome ${ctx.request.body.name}, We're Extremely Happy to Have You Join Us! Our Journey Has Only Just Begun.We'll Be Here To Kkary You Along The Whole Way.  .`,
+        html: `Hello ${ctx.request.body.name}, I'm Extremely Happy to Have You Here At The Start of A New Chapter. Our Journey Has Only Just Begun and I'll Be Right Beside You Telling The Story of This Wonderful Experience. Feel Free To Reach Out To Me With Your Ideas & Projects`,
       });
+      console.log("Leaving");
     }
-    return sanitizeEntity(entity, { model: strapi.models.live_alerts });
+    return sanitizeEntity(entity, { model: strapi.models.newsletter });
   },
 };
