dbd42f4d0993237a6e1eec01905d14714956b7f3
Vino Samari <iamvinomusic@gmail.com>
2021-09-15 13:54:33 +0000

    Set up nodemailer and email template - UNTESTED

--- api/live-alerts/controllers/live-alerts.js
+++ api/live-alerts/controllers/live-alerts.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.live_alerts.create(data, { files });
+    } else {
+      entity = await strapi.services.live_alerts.create(ctx.request.body);
+      await strapi.plugins["email"].services.email.send({
+        to: `${ctx.request.body.email}`,
+        subject: `🎉🎉🎉  Hey ${ctx.request.body.name} Let's Bring Your Ideas To Life TODAY! 🎉🎉🎉`,
+        text: `Hello ${ctx.request.body.name}, I'm Extremely Happy to Have You Here At The Start of A New Chapter. Our Journey Has Only Just Begun and I'll Be Right Beside You Telling The Story of This Wonderful Experience. Feel Free To Reach Out To Me With Your Ideas & Projects`,
+        html: `Welcome ${ctx.request.body.name}, We're Extremely Happy to Have You Join Us! Our Journey Has Only Just Begun.We'll Be Here To Kkary You Along The Whole Way.  .`,
+      });
+    }
+    return sanitizeEntity(entity, { model: strapi.models.live_alerts });
+  },
+};
--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "serahjohnsoncluster.uemjx.mongodb.net"),
+        host: env("DATABASE_HOST", "serahjohnsonapi.uemjx.mongodb.net"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
         database: env("DATABASE_NAME", "serahjohnsonbackend"),
